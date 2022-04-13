180aafd5d8bab381c69dc48625206590b020bfbd
berkc <b.cal.2193@gmail.com>
2022-04-13 17:49:32 +0200

    13.04

---
+++ src/meeting/Q20.java
@@ -0,0 +1,125 @@
+package meeting;
+
+import java.util.Scanner;
+
+public class Q20 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayiyi giriniz = ");
+        int sayi = scan.nextInt();
+
+        int birlerBasamagi = sayi % 10;
+        int onlarBasamagi = (sayi / 10) % 10;
+        int yüzlerBasamagi = (sayi / 100);
+
+
+        if (sayi > 99 && sayi < 1000) {
+
+
+            switch (yüzlerBasamagi) {
+                case 0:
+                    System.out.println("");
+                    break;
+                case 1:
+                    System.out.println("yüz");
+                    break;
+                case 2:
+                    System.out.println("ikiyüz");
+                    break;
+                case 3:
+                    System.out.println("ücyüz");
+                    break;
+                case 4:
+                    System.out.println("dörtyüz");
+                    break;
+                case 5:
+                    System.out.println("besyüz");
+                    break;
+                case 6:
+                    System.out.println("altiyüz");
+                    break;
+                case 7:
+                    System.out.println("yediyüz");
+                    break;
+                case 8:
+                    System.out.println("sekizyüz");
+                    break;
+                case 9:
+                    System.out.println("dokuzyüz");
+                    break;
+            }
+
+
+            switch (onlarBasamagi) {
+                case 0:
+                    System.out.println("");
+                    break;
+                case 1:
+                    System.out.println("on");
+                    break;
+                case 2:
+                    System.out.println("yirmi");
+                    break;
+                case 3:
+                    System.out.println("otuz");
+                    break;
+                case 4:
+                    System.out.println("kirk");
+                    break;
+                case 5:
+                    System.out.println("elli");
+                    break;
+                case 6:
+                    System.out.println("altmis");
+                    break;
+                case 7:
+                    System.out.println("yetmis");
+                    break;
+                case 8:
+                    System.out.println("seksen");
+                    break;
+                case 9:
+                    System.out.println("doksan");
+                    break;
+            }
+
+
+            switch (birlerBasamagi) {
+                case 0:
+                    System.out.println("");
+                    break;
+                case 1:
+                    System.out.println("bir");
+                    break;
+                case 2:
+                    System.out.println("iki");
+                    break;
+                case 3:
+                    System.out.println("uc");
+                    break;
+                case 4:
+                    System.out.println("dort");
+                    break;
+                case 5:
+                    System.out.println("bes");
+                    break;
+                case 6:
+                    System.out.println("alti");
+                    break;
+                case 7:
+                    System.out.println("yedi");
+                    break;
+                case 8:
+                    System.out.println("sekiz");
+                    break;
+                case 9:
+                    System.out.println("dokuz");
+                    break;
+            }
+        }
+
+
+    }//main
+
+}
