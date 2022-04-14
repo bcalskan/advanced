aa2471556c779225977bc7ba225fe687328c2b9f
berkc <b.cal.2193@gmail.com>
2022-04-14 10:16:02 +0200

    newProject

--- src/meetingQ/Arrays12_2.java
+++ src/meetingQ/Arrays12_2.java
@@ -1,5 +1,8 @@
 package meetingQ;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Arrays12_2 {
     public static void main(String[] args) {
 
@@ -10,6 +13,29 @@ public class Arrays12_2 {
     =>ikinci yolu da temp kullanarak cozun
     ipucu=>bir temp olusturup son elemani ile indekse atma ve bunu length bitene kadar devam etme
      */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Eleman sayisini giriniz : ");
+
+
+        int n = scan.nextInt();
+        int[] arr = new int[n]; //---> n elemanli bir array olusturuldu
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + 1 + ". elemani giriniz : ");
+            int eleman = scan.nextInt();
+            arr[i] = eleman;
+        }
+
+        System.out.println("Arr : " + Arrays.toString(arr)); //Arr : [12, 20, 25, 48, 56, 55] ---> length 6
+
+        int temp[] = new int[arr.length]; // arr uzunlugunda bir array olusturuldu
+
+        for (int i = temp.length - 1; i >= 0; i--) {  // 5
+            temp[i] = arr[temp.length - i - 1];
+            // temp[5] = arr[6-5-1] --- arr[0]
+        }
+        System.out.println("Temp-Ters : " + Arrays.toString(temp)); //Temp-Ters : [55, 56, 48, 25, 20, 12]
 
 
 
--- src/meetingQ/UcusSorusu.java
+++ src/meetingQ/UcusSorusu.java
@@ -1,24 +1,136 @@
 package meetingQ;
 
+import java.util.Scanner;
+
 public class UcusSorusu {
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-          /*
-    A şehrinden uçmak isteyen bir yolcu
-    B şehrine 500km
-    C şehrine  700km
-    D şehrine  900 km mesafededir.
+        System.out.println(" ***** Firmamiza hos geldiniz. *****");
 
-    Bilet tarifesi:
-    km birim fiyati : 0.10$
+        System.out.print("Lütfen B-C-D sehirlerinden gitmek istediginiz sehri seciniz : ");
 
-    yolcu 12 yasindan kucukse toplam fiyat %50 indirim,
-    12 ve 24 yas arasindaysa 10% indirim,
-    65 yasindan buyukse 30% indirim,
-    bilet gidis donus alinirrsa 20% indirim uygulayan bi app create ediniz
+        char sehir = scan.next().toUpperCase().charAt(0); // char sehir B
 
- */
+        System.out.print("Tek yön bilet almak icin T´ye \nCift yön bilet almak icin C´ye basiniz.");
+        char biletYön = scan.next().toUpperCase().charAt(0);
 
+        System.out.print("Lütfen yasinizi giriniz : ");
+        int yas = scan.nextInt();
+
+        double ücretB = 500 * 0.10;
+        double ücretC = 700 * 0.10;
+        double ücretD = 900 * 0.10;
+
+        if (sehir == 'B' || sehir == 'C' || sehir == 'D') {
+
+            if (yas > 65) {
+                if (sehir == 'B') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretB * 0.7 + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretB * 2 * 0.7) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }                       //3.if
+
+                if (sehir == 'C') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretC * 0.7 + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretC * 2 * 0.7) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }
+
+                if (sehir == 'D') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretD * 0.7 + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretD * 2 * 0.7) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }                       //3.if
+
+
+            } else if (yas >= 12 && yas < 24) {
+                if (sehir == 'B') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretB * 0.9 + " $");
+                    } else if (biletYön == 'C') {
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretB * 2 * 0.9) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }
+
+                if (sehir == 'C') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretC * 0.9 + " $");
+                    } else if (biletYön == 'C') {
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretC * 2 * 0.9) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }
+
+                if (sehir == 'D') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretD * 0.9 + " $");
+                    } else if (biletYön == 'C') {
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretD * 2 * 0.9) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }
+
+
+            } else if (yas < 12) {
+                if (sehir == 'B') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretB * 0.5 + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretB * 2 * 0.5) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }                       //3.if
+
+                if (sehir == 'C') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretC * 0.5 + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretC * 2 * 0.5) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }
+
+                if (sehir == 'D') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretB * 0.5 + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretB * 2 * 0.5) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }                       //3.if
+
+
+            } else if (yas >= 24 && yas <= 65) {
+                if (sehir == 'B') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretB + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretB * 2) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }                       //3.if
+
+                if (sehir == 'C') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretC + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretC * 2) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }
+
+                if (sehir == 'D') {
+                    if (biletYön == 'T') {
+                        System.out.println("Ödemeniz gereken miktar : " + ücretB + " $");
+                    } else if (biletYön == 'C') {  //4.if
+                        System.out.println("Ödemeniz gereken miktar : " + (ücretB * 2) * 0.80 + " $");
+                    } else System.out.println("Hatali giris yaptiniz");
+                }
+            }
+
+
+        } else System.out.println("Girdiginiz sehire seferimiz bulunmamaktadir."); // 1.if
 
 
     }
